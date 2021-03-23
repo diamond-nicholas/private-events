dad55a93caf42d734df946582c2d8cbc21906ae4
diamond-nicholas <diamondnicholas154@gmail.com>
2021-03-23 21:49:47 +0100

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :events
-  devise_for :users only: [new:, create:, show:]
-
-  root "Events#index"
+  resources :events, only: %i[new create show]
+  devise_for :users 
+  root "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
