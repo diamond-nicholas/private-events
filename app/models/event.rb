ebeea2e09e7ab7f130f440a13de8353df0b7d0d7
franklin <franklinbenjamin032@gmail.com>
2021-03-26 13:51:27 -0400

    add past and future events to user show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances
+
+  def self.past_event
+    event.where("date > ?", DateTime.now)
+  end
+
+  def self.future_event
+    event.where("date < ?", DateTime.now)
+  end
 end
