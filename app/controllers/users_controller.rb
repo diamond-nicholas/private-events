d1e18fd5ddb0e5ca9eec1696bac40f1ea3edfaa4
franklin <franklinbenjamin032@gmail.com>
2021-03-29 11:03:35 -0400

    add events created by user, remove logic from views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = @user.events.all
+    @past_events = @user.attended_events.past
+    @upcoming_events = @user.attended_events.future
   end
 end
